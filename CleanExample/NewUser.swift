0698bd848198a3049f6449b4e3fd75a41025a647
Felipe Ruz <feliperuzg@gmail.com>
2017-07-18 16:05:44 -0400

    Initial commit

---
+++ CleanExample/NewUser.swift
@@ -0,0 +1,9 @@
+//
+//  NewUser.swift
+//  CleanExample
+//
+//  Created by Felipe Ruz on 18-07-17.
+//  Copyright © 2017 Felipe Ruz. All rights reserved.
+//
+
+import Foundation
---
+++ CleanExample/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  CleanExample
+//
+//  Created by Felipe Ruz on 18-07-17.
+//  Copyright © 2017 Felipe Ruz. All rights reserved.
+//
+
+protocol User {
+    var firstName: String { get }
+    var lastName: String { get }
+    var age: String { get }
+    var phone: String { get }
+    var email: String { get }
+    var address: String { get }
+}

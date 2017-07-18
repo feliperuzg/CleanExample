f2a7e15a8d6edcfe59156bd7f7553465e9ef6849
Felipe Ruz <fruz@mobdev.cl>
2017-07-18 15:18:17 -0400

    Initial Commit

---
+++ CleanExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanExample
+//
+//  Created by Felipe Ruz on 18-07-17.
+//  Copyright © 2017 Felipe Ruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

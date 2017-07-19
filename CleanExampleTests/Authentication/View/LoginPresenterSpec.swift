24d80378c00de7a50ed14643523f109ccfa1bf82
Felipe Ruz <feliperuzg@gmail.com>
2017-07-19 17:53:38 -0400

    Presenter Tests

--- CleanExampleTests/Authentication/View/LoginPresenterSpec.swift
+++ CleanExampleTests/Authentication/View/LoginPresenterSpec.swift
@@ -8,20 +8,85 @@
 
 import XCTest
 @testable import CleanExample
+
+class LoginViewMock: NSObject, LoginViewProtocol {
+    var showActivityCalled = false
+    var hideActivityCalled = false
+    var showErrorCalled = false
+
+    func showActivityIndicator() {
+        showActivityCalled = true
+    }
+
+    func hideActivityIndicator() {
+        hideActivityCalled = true
+    }
+
+    func showErrorMessage(_ error: CustomError) {
+        showErrorCalled = true
+    }
+}
+
+class LoginApiMock: LoginMockApi {
+    override func loginUser(withCredentials userName: String, password: String,
+                            onSuccess: @escaping (UserEntity) -> Void,
+                            onError: @escaping (CustomError) -> Void) {
+        if userName.isEmpty || password.isEmpty {
+            onError(error)
+        } else {
+            onSuccess(UserEntity(fromDictionary: response))
+        }
+    }
+}
+
+class MockServiceLocator: LoginServiceLocator {
+    override var restApi: LoginRestApi {
+        return LoginApiMock()
+    }
+}
+
 class LoginPresenterSpec: XCTestCase {
     var sut: LoginPresenterProtocol!
     let locator = LoginServiceLocator()
-    override func setUp() {
-        super.setUp()
-        sut = LoginPresenter(locator.useCases)
-    }
+    let fakeLocator = MockServiceLocator()
 
     override func tearDown() {
         super.tearDown()
         sut = nil
     }
 
-    func presenterCanInitilize() {
+    func testPresenterCanInitilize() {
+        sut = LoginPresenter(locator.useCases)
         XCTAssertNotNil(sut)
+    }
+
+    func testPresenterCalldoLogin() {
+        sut = LoginPresenter(locator.useCases)
+        let viewController = LoginViewMock()
+        sut.attachView(viewController)
+
+        sut.doLogin("Juan", password: "1234")
+
+        XCTAssertTrue(viewController.showActivityCalled)
+    }
+
+    func testPresenterHidesIndicatorView() {
+        sut = LoginPresenter(fakeLocator.useCases)
+        let viewController = LoginViewMock()
+        sut.attachView(viewController)
+
+        sut.doLogin("", password: "")
+
+        XCTAssertTrue(viewController.hideActivityCalled)
+    }
+
+    func testPresenterDisplaysAlertViewOnError() {
+        sut = LoginPresenter(fakeLocator.useCases)
+        let viewController = LoginViewMock()
+        sut.attachView(viewController)
+
+        sut.doLogin("", password: "")
+
+        XCTAssertTrue(viewController.showErrorCalled)
     }
 }
